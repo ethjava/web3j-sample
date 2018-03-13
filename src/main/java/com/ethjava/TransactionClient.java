b4059ec504162665b83daddf726a176e763e5e1e
totoroYang <yzw5535@gmail.com>
2018-03-13 23:07:11 +0800

    add transaction

---
+++ src/main/java/com/ethjava/TransactionClient.java
@@ -0,0 +1,143 @@
+package com.ethjava;
+
+import com.ethjava.utils.Environment;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.admin.Admin;
+import org.web3j.protocol.admin.methods.response.PersonalUnlockAccount;
+import org.web3j.protocol.core.DefaultBlockParameterName;
+import org.web3j.protocol.core.methods.request.Transaction;
+import org.web3j.protocol.core.methods.response.EthEstimateGas;
+import org.web3j.protocol.core.methods.response.EthGetBalance;
+import org.web3j.protocol.core.methods.response.EthGetTransactionCount;
+import org.web3j.protocol.core.methods.response.EthSendTransaction;
+import org.web3j.protocol.http.HttpService;
+import org.web3j.utils.Convert;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+public class TransactionClient {
+	private static Web3j web3j;
+	private static Admin admin;
+
+	private static String fromAddress = "0x7b1cc408fcb2de1d510c1bf46a329e9027db4112";
+	private static String toAddress = "0x05f50cd5a97d9b3fec35df3d0c6c8234e6793bdf";
+	private static BigDecimal defaultGasPrice = BigDecimal.valueOf(5);
+
+	public static void main(String[] args) {
+		web3j = Web3j.build(new HttpService(Environment.RPC_URL));
+		admin = Admin.build(new HttpService(Environment.RPC_URL));
+
+		getBalance(fromAddress);
+		sendTransaction();
+	}
+
+	/**
+	 * 获取余额
+	 *
+	 * @param address 钱包地址
+	 * @return 余额
+	 */
+	private static BigInteger getBalance(String address) {
+		BigInteger balance = null;
+		try {
+			EthGetBalance ethGetBalance = web3j.ethGetBalance(address, DefaultBlockParameterName.LATEST).send();
+			balance = ethGetBalance.getBalance();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("address " + address + " balance " + balance + "wei");
+		return balance;
+	}
+
+	/**
+	 * 生成一个普通交易对象
+	 *
+	 * @param fromAddress 放款方
+	 * @param toAddress   收款方
+	 * @param nonce       交易序号
+	 * @param gasPrice    gas 价格
+	 * @param gasLimit    gas 数量
+	 * @param value       金额
+	 * @return 交易对象
+	 */
+	private static Transaction makeTransaction(String fromAddress, String toAddress,
+											   BigInteger nonce, BigInteger gasPrice,
+											   BigInteger gasLimit, BigInteger value) {
+		Transaction transaction;
+		transaction = Transaction.createEtherTransaction(fromAddress, nonce, gasPrice, gasLimit, toAddress, value);
+		return transaction;
+	}
+
+	/**
+	 * 获取普通交易的gas上限
+	 *
+	 * @param transaction 交易对象
+	 * @return gas 上限
+	 */
+	private static BigInteger getTransactionGasLimit(Transaction transaction) {
+		BigInteger gasLimit = BigInteger.ZERO;
+		try {
+			EthEstimateGas ethEstimateGas = web3j.ethEstimateGas(transaction).send();
+			gasLimit = ethEstimateGas.getAmountUsed();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return gasLimit;
+	}
+
+	/**
+	 * 获取账号交易次数 nonce
+	 *
+	 * @param address 钱包地址
+	 * @return nonce
+	 */
+	private static BigInteger getTransactionNonce(String address) {
+		BigInteger nonce = BigInteger.ZERO;
+		try {
+			EthGetTransactionCount ethGetTransactionCount = web3j.ethGetTransactionCount(address, DefaultBlockParameterName.LATEST).send();
+			nonce = ethGetTransactionCount.getTransactionCount();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return nonce;
+	}
+
+	/**
+	 * 发送一个普通交易
+	 *
+	 * @return 交易 Hash
+	 */
+	private static String sendTransaction() {
+		String password = "yzw";
+		BigInteger unlockDuration = BigInteger.valueOf(60L);
+		BigDecimal amount = new BigDecimal("0.01");
+		String txHash = null;
+		try {
+			PersonalUnlockAccount personalUnlockAccount = admin.personalUnlockAccount(fromAddress, password, unlockDuration).send();
+			if (personalUnlockAccount.accountUnlocked()) {
+				BigInteger value = Convert.toWei(amount, Convert.Unit.ETHER).toBigInteger();
+				Transaction transaction = makeTransaction(fromAddress, toAddress,
+						null, null, null, value);
+				//不是必须的 可以使用默认值
+				BigInteger gasLimit = getTransactionGasLimit(transaction);
+				//不是必须的 缺省值就是正确的值
+				BigInteger nonce = getTransactionNonce(fromAddress);
+				//该值为大部分矿工可接受的gasPrice
+				BigInteger gasPrice = Convert.toWei(defaultGasPrice, Convert.Unit.GWEI).toBigInteger();
+				transaction = makeTransaction(fromAddress, toAddress,
+						nonce, gasPrice,
+						gasLimit, value);
+				EthSendTransaction ethSendTransaction = web3j.ethSendTransaction(transaction).send();
+				txHash = ethSendTransaction.getTransactionHash();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("tx hash " + txHash);
+		return txHash;
+	}
+
+	// TODO: 2018/3/13 使用 web3j.ethSendRawTransaction() 发送交易 需要用私钥自签名交易
+}
