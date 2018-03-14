9c69ea0edd53757f691e5aec2bbc11cdde5cd26d
totoroYang <yzw5535@gmail.com>
2018-03-14 22:58:28 +0800

    add security

--- src/main/java/com/ethjava/TokenClient.java
+++ src/main/java/com/ethjava/TokenClient.java
@@ -7,7 +7,9 @@ import org.web3j.abi.TypeReference;
 import org.web3j.abi.datatypes.Address;
 import org.web3j.abi.datatypes.Function;
 import org.web3j.abi.datatypes.Type;
+import org.web3j.abi.datatypes.Utf8String;
 import org.web3j.abi.datatypes.generated.Uint256;
+import org.web3j.abi.datatypes.generated.Uint8;
 import org.web3j.protocol.Web3j;
 import org.web3j.protocol.core.DefaultBlockParameterName;
 import org.web3j.protocol.core.methods.request.Transaction;
@@ -18,6 +20,7 @@ import java.io.IOException;
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 /**
  * 基于ERC20的代币
@@ -30,15 +33,19 @@ public class TokenClient {
 
 	private static String contractAddress = "0x4c1ae77bc2df45fb68b13fa1b4f000305209b0cb";
 
+	private static String emptyAddress = "0x0000000000000000000000000000000000000000";
+
 	public static void main(String[] args) {
 		web3j = Web3j.build(new HttpService(Environment.RPC_URL));
-		getTokenBalance();
+		getTokenBalance(fromAddress, contractAddress);
+		System.out.println(getTokenName(contractAddress));
+		System.out.println(getTokenDeDecimals(contractAddress));
 	}
 
 	/**
 	 * 查询代币余额
 	 */
-	private static void getTokenBalance() {
+	private static BigInteger getTokenBalance(String fromAddress, String contractAddress) {
 
 		String methodName = "balanceOf";
 		List<Type> inputParameters = new ArrayList<>();
@@ -54,14 +61,70 @@ public class TokenClient {
 		Transaction transaction = Transaction.createEthCallTransaction(fromAddress, contractAddress, data);
 
 		EthCall ethCall;
+		BigInteger balanceValue = BigInteger.ZERO;
 		try {
 			ethCall = web3j.ethCall(transaction, DefaultBlockParameterName.LATEST).send();
 			List<Type> results = FunctionReturnDecoder.decode(ethCall.getValue(), function.getOutputParameters());
-			BigInteger balanceValue = (BigInteger) results.get(0).getValue();
+			balanceValue = (BigInteger) results.get(0).getValue();
 			String balance = balanceValue.toString();
 			System.out.println(balance);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		return balanceValue;
+	}
+
+	private static String getTokenName(String contractAddress) {
+		String methodName = "name";
+		String name = null;
+		String fromAddr = emptyAddress;
+		List<Type> inputParameters = new ArrayList<>();
+		List<TypeReference<?>> outputParameters = new ArrayList<>();
+
+		TypeReference<Utf8String> typeReference = new TypeReference<Utf8String>() {
+		};
+		outputParameters.add(typeReference);
+
+		Function function = new Function(methodName, inputParameters, outputParameters);
+
+		String data = FunctionEncoder.encode(function);
+		Transaction transaction = Transaction.createEthCallTransaction(fromAddr, contractAddress, data);
+
+		EthCall ethCall;
+		try {
+			ethCall = web3j.ethCall(transaction, DefaultBlockParameterName.LATEST).sendAsync().get();
+			List<Type> results = FunctionReturnDecoder.decode(ethCall.getValue(), function.getOutputParameters());
+			name = results.get(0).getValue().toString();
+		} catch (InterruptedException | ExecutionException e) {
+			e.printStackTrace();
+		}
+		return name;
+	}
+
+	private static int getTokenDeDecimals(String contractAddress) {
+		String methodName = "decimals";
+		String fromAddr = emptyAddress;
+		int decimal = 0;
+		List<Type> inputParameters = new ArrayList<>();
+		List<TypeReference<?>> outputParameters = new ArrayList<>();
+
+		TypeReference<Uint8> typeReference = new TypeReference<Uint8>() {
+		};
+		outputParameters.add(typeReference);
+
+		Function function = new Function(methodName, inputParameters, outputParameters);
+
+		String data = FunctionEncoder.encode(function);
+		Transaction transaction = Transaction.createEthCallTransaction(fromAddr, contractAddress, data);
+
+		EthCall ethCall;
+		try {
+			ethCall = web3j.ethCall(transaction, DefaultBlockParameterName.LATEST).sendAsync().get();
+			List<Type> results = FunctionReturnDecoder.decode(ethCall.getValue(), function.getOutputParameters());
+			decimal = Integer.parseInt(results.get(0).getValue().toString());
+		} catch (InterruptedException | ExecutionException e) {
+			e.printStackTrace();
+		}
+		return decimal;
 	}
 }
