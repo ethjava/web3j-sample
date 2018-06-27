3d52bacc78bcd700ad52c608115d4bb232530acb
totoroYang <yzw5535@gmail.com>
2018-06-27 18:38:12 +0800

    add

--- src/main/java/com/ethjava/DecodeMessage.java
+++ src/main/java/com/ethjava/DecodeMessage.java
@@ -1,9 +1,6 @@
 package com.ethjava;
 
-import org.web3j.crypto.Keys;
-import org.web3j.crypto.RawTransaction;
-import org.web3j.crypto.Sign;
-import org.web3j.crypto.TransactionEncoder;
+import org.web3j.crypto.*;
 import org.web3j.rlp.RlpDecoder;
 import org.web3j.rlp.RlpList;
 import org.web3j.rlp.RlpString;
@@ -11,6 +8,7 @@ import org.web3j.rlp.RlpType;
 import org.web3j.utils.Numeric;
 
 import java.math.BigInteger;
+import java.security.SignatureException;
 import java.util.List;
 
 public class DecodeMessage {
@@ -22,7 +20,7 @@ public class DecodeMessage {
 	public static void main(String[] args) {
 		String signedData = "0xf8ac8201518506fc23ac00830493e094fda023cea60a9f421d74ac49f9a015880a77dd7280b844a9059cbb000000000000000000000000b5dbd2e4093a501f1d1e645f04cef5815a1581d7000000000000000000000000000000000000000000000004c53ecdc18a6000001ca03d710f3c5aabde2733938c44c0b1448f96e760c030205562f59889557397faa4a007110abbcfa343381a2f713d6339d3fa751200f82cc2f06a4d1967b4eaf61d50";
 		decodeMessage(signedData);
-
+		decodeMessageV340(signedData);
 	}
 
 	private static void decodeMessage(String signedData) {
@@ -52,10 +50,24 @@ public class DecodeMessage {
 			e.printStackTrace();
 		}
 	}
+
 	/**
 	 * 可以看到交易数据本身是没有加密的，是可以直接获取到。
 	 * v r s是用私钥加密的数据，利用v r s加上交易数据可以得到私钥对应的公钥及地址。
 	 * 所以RawTransaction里是没有fromAddress的参数的。
 	 * 解密出的地址就是发出交易的地址。这样一来完成了验证。
 	 */
+	private static void decodeMessageV340(String signedData) {
+		System.out.println("解密 start " + System.currentTimeMillis());
+		RawTransaction rawTransaction = TransactionDecoder.decode(signedData);
+		if (rawTransaction instanceof SignedRawTransaction) {
+			try {
+				String from = ((SignedRawTransaction) rawTransaction).getFrom();
+				System.out.println("address " + from);
+			} catch (SignatureException e) {
+				e.printStackTrace();
+			}
+		}
+		System.out.println("解密 end " + System.currentTimeMillis());
+	}
 }
